7b055424a8c97ecef4fdd468a797859667a35c2d
bilalramxan <bilalramzan7326@gmail.com>
2023-04-15 19:54:18 +0500

    Added testing file of map

---
+++ lib/maps/map_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MapTest extends StatefulWidget {
+  const MapTest({super.key});
+
+  @override
+  State<MapTest> createState() => _MapTestState();
+}
+
+class _MapTestState extends State<MapTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
